d890130dcd4d9d0566bf226697c177b5ac805159
LeunamNegrete <manuelnegretegarrido@gmail.com>
2023-07-01 13:16:29 +0000

    ahora corre, faltaba definir atributos a una gema

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,10 @@
 class Tweet < ApplicationRecord
-end
+    # ...
+  
+    def self.ransackable_attributes(auth_object = nil)
+      ["created_at", "description", "id", "updated_at", "user_name"]
+    end
+  
+    # ...
+  end
+  
